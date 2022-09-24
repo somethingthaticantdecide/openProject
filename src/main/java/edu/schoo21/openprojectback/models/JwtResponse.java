9cf37852ed41ab2a307f7afa9cea16380c11d2f3
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2022-09-24 21:26:35 +0300

    Поменял /signin /signUp методы

--- src/main/java/edu/schoo21/openprojectback/models/JwtResponse.java
+++ src/main/java/edu/schoo21/openprojectback/models/JwtResponse.java
@@ -1,16 +1,38 @@
 package edu.schoo21.openprojectback.models;
 
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class JwtResponse {
 
 	private final String jwttoken;
-	private final User user;
+	private Long id;
+	private String login;
+	private String name;
+	private Long phoneNumber;
+	private String mail;
+	private String address;
+	private String avatar;
+	private Float ranking;
+	private List<Feedback> feedbacks;
+	private List<Cat> cats;
+	private List<Cat> favorites;
+	private List<Chat> chats;
 
 	public JwtResponse(String jwttoken, User user) {
 		this.jwttoken = jwttoken;
-		this.user = user;
+		this.login = user.getLogin();
+		this.name = user.getName();
+		this.phoneNumber = user.getPhoneNumber();
+		this.mail = user.getMail();
+		this.address = user.getAddress();
+		this.avatar = user.getAvatar();
+		this.ranking = user.getRanking();
+		this.feedbacks = user.getFeedbacks();
+		this.cats = user.getCats();
+		this.favorites = user.getFavorites();
+		this.chats = user.getChats();
 	}
 
 }
