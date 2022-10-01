380b0be1b19a01729fbcfc6a5cdfce8224a492c7
aanigmatov <aanigmatov@sberbank.ru>
2022-10-01 18:57:34 +0300

    add @CrossOrigin

--- src/main/java/edu/schoo21/openprojectback/config/WebSecurityConfig.java
+++ src/main/java/edu/schoo21/openprojectback/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package edu.schoo21.openprojectback.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -47,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
+				.cors().and()
 				// dont authenticate this particular request
 				.authorizeRequests()
 				.antMatchers("/authenticate").permitAll()
