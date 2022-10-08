e54210cb23a9c9a8a62091e0f4901f8fbedd9033
somethingthaticantdecide <wmiprfpPCbBAi2s>
2022-10-08 21:37:03 +0300

    minor fixes

--- src/main/java/edu/schoo21/openprojectback/controllers/UsersController.java
+++ src/main/java/edu/schoo21/openprojectback/controllers/UsersController.java
@@ -22,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -118,7 +119,9 @@ public class UsersController {
     @GetMapping("/{user-id}/feedbacks")
     @ResponseStatus(HttpStatus.OK)
     public Collection<Feedback> getFeedbacksByUser(@PathVariable("user-id") Long id) {
-        return usersService.findById(id).getFeedbacks();
+        List<Feedback> feedbackList = usersService.findById(id).getFeedbacks();
+        feedbackList.forEach(feedbackService::updateOwnerImage);
+        return feedbackList;
     }
 
     @PostMapping("/{user-id}/feedbacks")
